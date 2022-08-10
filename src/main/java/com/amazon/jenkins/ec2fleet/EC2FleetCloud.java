fe0759ac4b690258af5ce0ef6a46df2495ba4b65
Hubert Szymon Okoń <hubertszymonokon@gmail.com>
2022-08-10 21:08:28 +0200

    Fix auto import change

--- src/main/java/com/amazon/jenkins/ec2fleet/EC2FleetCloud.java
+++ src/main/java/com/amazon/jenkins/ec2fleet/EC2FleetCloud.java
@@ -30,7 +30,16 @@ import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
