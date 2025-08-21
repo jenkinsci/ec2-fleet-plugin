1307043c4cde5fd3a45836afb25fc652caf74e18
Brady Shober <32466298+BradyShober@users.noreply.github.com>
2025-08-21 04:28:49 +0000

    I was basing off an outdated test

--- src/test/java/com/amazon/jenkins/ec2fleet/EC2FleetStatsApiTest.java
+++ src/test/java/com/amazon/jenkins/ec2fleet/EC2FleetStatsApiTest.java
@@ -1,13 +1,13 @@
+java
 package com.amazon.jenkins.ec2fleet;
 
-import com.amazon.jenkins.ec2fleet.EC2FleetCloud;
 import com.amazon.jenkins.ec2fleet.FleetStateStats;
-import hudson.slaves.Cloud;
+import com.amazon.jenkins.ec2fleet.EC2FleetCloud;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.Rule;
-import org.mockito.Mockito;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+import org.mockito.Mockito;
 
 import java.io.InputStream;
 import java.net.URL;
@@ -18,29 +18,30 @@ import java.util.HashSet;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class EC2FleetStatsApiTest {
+@WithJenkins
+class EC2FleetStatsApiTest {
 
-    @Rule
-    public JenkinsRule jenkinsRule = new JenkinsRule();
+    private JenkinsRule j;
+
+    @BeforeEach
+    void before(JenkinsRule rule) {
+        j = rule;
+    }
 
     @Test
-    public void testFleetStatsApiEndpointReturnsExpectedJson() throws Exception {
-        // Mock FleetStateStats
+    void testFleetStatsApiEndpointReturnsExpectedJson() throws Exception {
         FleetStateStats.State state = new FleetStateStats.State(true, false, "active");
         FleetStateStats stats = new FleetStateStats("fleet-1", 2, state,
                 new HashSet<>(Arrays.asList("i-1", "i-2")), Collections.emptyMap());
 
-        // Mock EC2FleetCloud
         EC2FleetCloud cloud = Mockito.mock(EC2FleetCloud.class);
         Mockito.when(cloud.getStats()).thenReturn(stats);
         Mockito.when(cloud.getFleet()).thenReturn("fleet-1");
         Mockito.when(cloud.getLabelString()).thenReturn("label-1");
 
-        // Add mock cloud to Jenkins
-        jenkinsRule.jenkins.clouds.add(cloud);
+        j.jenkins.clouds.add(cloud);
 
-        // Make HTTP request to the endpoint
-        String url = jenkinsRule.getURL() + "ec2-fleet/stats";
+        String url = j.getURL() + "ec2-fleet/stats";
         try (InputStream is = new URL(url).openStream()) {
             String json = new String(is.readAllBytes(), StandardCharsets.UTF_8);
             assertTrue(json.contains("\"fleet\":\"fleet-1\""));
