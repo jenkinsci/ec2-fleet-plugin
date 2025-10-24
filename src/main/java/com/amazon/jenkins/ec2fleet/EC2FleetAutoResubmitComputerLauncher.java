fee9df6cd9d9da79176f67234f54b0fa81c2c022
Arend Lagendijk <a.lagendijk@minddistrict.com>
2025-10-24 15:06:12 +0200

    fix resubmit logic, use getLastBuild ()instead of getLastFailedBuild()

--- src/main/java/com/amazon/jenkins/ec2fleet/EC2FleetAutoResubmitComputerLauncher.java
+++ src/main/java/com/amazon/jenkins/ec2fleet/EC2FleetAutoResubmitComputerLauncher.java
@@ -102,8 +102,8 @@ public class EC2FleetAutoResubmitComputerLauncher extends DelegatingComputerLaun
 
                     final List<Action> actions = new ArrayList<>();
                     if (task instanceof WorkflowJob) {
-                        final WorkflowRun failedBuild = ((WorkflowJob) task).getLastFailedBuild();
-                        actions.addAll(failedBuild.getActions(ParametersAction.class));
+                        final WorkflowRun lastBuild = ((WorkflowJob) task).getLastBuild();
+                        actions.addAll(lastBuild.getActions(ParametersAction.class));
                     }
                     if (executable instanceof Actionable) {
                         actions.addAll(((Actionable) executable).getAllActions());
--- src/test/java/com/amazon/jenkins/ec2fleet/EC2FleetAutoResubmitComputerLauncherTest.java
+++ src/test/java/com/amazon/jenkins/ec2fleet/EC2FleetAutoResubmitComputerLauncherTest.java
@@ -42,174 +42,174 @@ import static org.mockito.Mockito.withSettings;
 @MockitoSettings(strictness = Strictness.LENIENT)
 class EC2FleetAutoResubmitComputerLauncherTest {
 
-    private MockedStatic<Jenkins> mockedJenkins;
+  private MockedStatic<Jenkins> mockedJenkins;
 
-    private MockedStatic<Queue> mockedQueue;
+  private MockedStatic<Queue> mockedQueue;
 
-    @Mock
-    private ComputerLauncher baseComputerLauncher;
+  @Mock
+  private ComputerLauncher baseComputerLauncher;
 
-    @Mock
-    private TaskListener taskListener;
+  @Mock
+  private TaskListener taskListener;
 
-    @Mock
-    private Action action1;
+  @Mock
+  private Action action1;
 
-    @Mock
-    private Executor executor1;
+  @Mock
+  private Executor executor1;
 
-    @Mock
-    private Executor executor2;
+  @Mock
+  private Executor executor2;
 
-    private Actionable executable1;
+  private Actionable executable1;
 
-    @Mock
-    private Queue.Executable executable2;
+  @Mock
+  private Queue.Executable executable2;
 
-    @Mock
-    private Slave agent;
+  @Mock
+  private Slave agent;
 
-    @Mock
-    private EC2FleetNodeComputer computer;
+  @Mock
+  private EC2FleetNodeComputer computer;
 
-    @Mock
-    private Jenkins jenkins;
+  @Mock
+  private Jenkins jenkins;
 
-    @Mock
-    private Queue queue;
+  @Mock
+  private Queue queue;
 
-    @Mock
-    private SubTask subTask1;
+  @Mock
+  private SubTask subTask1;
 
-    @Mock
-    private SubTask subTask2;
+  @Mock
+  private SubTask subTask2;
 
-    @Mock
-    private Queue.Task task1;
+  @Mock
+  private Queue.Task task1;
 
-    @Mock
-    private Queue.Task task2;
+  @Mock
+  private Queue.Task task2;
 
-    @Mock
-    private EC2FleetNode fleetNode;
+  @Mock
+  private EC2FleetNode fleetNode;
 
-    @Mock
-    private EC2FleetCloud cloud;
+  @Mock
+  private EC2FleetCloud cloud;
 
-    @Mock
-    private WorkflowJob workflowJob;
+  @Mock
+  private WorkflowJob workflowJob;
 
-    @Mock
-    private WorkflowRun workflowRun;
+  @Mock
+  private WorkflowRun workflowRun;
 
-    @BeforeEach
-    void before() {
-        executable1 = mock(Actionable.class, withSettings().extraInterfaces(Queue.Executable.class));
+  @BeforeEach
+  void before() {
+    executable1 = mock(Actionable.class, withSettings().extraInterfaces(Queue.Executable.class));
 
-        when(computer.getDisplayName()).thenReturn("i-12");
+    when(computer.getDisplayName()).thenReturn("i-12");
 
-        mockedJenkins = Mockito.mockStatic(Jenkins.class);
-        mockedJenkins.when(Jenkins::get).thenReturn(jenkins);
-        mockedQueue = Mockito.mockStatic(Queue.class);
-        mockedQueue.when(Queue::getInstance).thenReturn(queue);
+    mockedJenkins = Mockito.mockStatic(Jenkins.class);
+    mockedJenkins.when(Jenkins::get).thenReturn(jenkins);
+    mockedQueue = Mockito.mockStatic(Queue.class);
+    mockedQueue.when(Queue::getInstance).thenReturn(queue);
 
-        when(agent.getNumExecutors()).thenReturn(1);
+    when(agent.getNumExecutors()).thenReturn(1);
 
-        when(fleetNode.getDisplayName()).thenReturn("fleet node name");
+    when(fleetNode.getDisplayName()).thenReturn("fleet node name");
 
-        when(((Queue.Executable) executable1).getParent()).thenReturn(subTask1);
-        when(executable2.getParent()).thenReturn(subTask2);
+    when(((Queue.Executable) executable1).getParent()).thenReturn(subTask1);
+    when(executable2.getParent()).thenReturn(subTask2);
 
-        when(subTask1.getOwnerTask()).thenReturn(task1);
-        when(subTask2.getOwnerTask()).thenReturn(task2);
+    when(subTask1.getOwnerTask()).thenReturn(task1);
+    when(subTask2.getOwnerTask()).thenReturn(task2);
 
-        when(executor1.getCurrentExecutable()).thenReturn((Queue.Executable) executable1);
-        when(executor2.getCurrentExecutable()).thenReturn(executable2);
+    when(executor1.getCurrentExecutable()).thenReturn((Queue.Executable) executable1);
+    when(executor2.getCurrentExecutable()).thenReturn(executable2);
 
-        when(computer.getExecutors()).thenReturn(Arrays.asList(executor1, executor2));
-        when(computer.isOffline()).thenReturn(true);
+    when(computer.getExecutors()).thenReturn(Arrays.asList(executor1, executor2));
+    when(computer.isOffline()).thenReturn(true);
 
-        when(computer.getCloud()).thenReturn(cloud);
-    }
+    when(computer.getCloud()).thenReturn(cloud);
+  }
 
-    @AfterEach
-    void after() {
-        mockedQueue.close();
-        mockedJenkins.close();
-    }
+  @AfterEach
+  void after() {
+    mockedQueue.close();
+    mockedJenkins.close();
+  }
 
-    @Test
-    void afterDisconnect_should_do_nothing_if_still_online() {
-        when(computer.isOffline()).thenReturn(false);
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-                .afterDisconnect(computer, taskListener);
-        verifyNoInteractions(queue);
-    }
+  @Test
+  void afterDisconnect_should_do_nothing_if_still_online() {
+    when(computer.isOffline()).thenReturn(false);
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verifyNoInteractions(queue);
+  }
 
-    @Test
-    void afterDisconnect_should_do_nothing_if_offline_but_no_executable() {
-        when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
-        when(executor1.getCurrentExecutable()).thenReturn(null);
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-                .afterDisconnect(computer, taskListener);
-        verifyNoInteractions(queue);
-    }
+  @Test
+  void afterDisconnect_should_do_nothing_if_offline_but_no_executable() {
+    when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
+    when(executor1.getCurrentExecutable()).thenReturn(null);
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verifyNoInteractions(queue);
+  }
 
-    @Test
-    void taskCompleted_should_resubmit_task_if_offline_and_has_executable() {
-        when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-                .afterDisconnect(computer, taskListener);
-        verify(queue).schedule2(eq(task1), anyInt(), eq(Collections.emptyList()));
-        verifyNoMoreInteractions(queue);
-    }
+  @Test
+  void taskCompleted_should_resubmit_task_if_offline_and_has_executable() {
+    when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verify(queue).schedule2(eq(task1), anyInt(), eq(Collections.emptyList()));
+    verifyNoMoreInteractions(queue);
+  }
 
-    @Test
-    void taskCompleted_should_not_resubmit_task_if_offline_but_disabled() {
-        when(cloud.isDisableTaskResubmit()).thenReturn(true);
-        when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-                .afterDisconnect(computer, taskListener);
-        verifyNoInteractions(queue);
-    }
+  @Test
+  void taskCompleted_should_not_resubmit_task_if_offline_but_disabled() {
+    when(cloud.isDisableTaskResubmit()).thenReturn(true);
+    when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verifyNoInteractions(queue);
+  }
 
-    @Test
-    void taskCompleted_should_resubmit_task_for_all_executors() {
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-                .afterDisconnect(computer, taskListener);
-        verify(queue).schedule2(eq(task1), anyInt(), eq(Collections.emptyList()));
-        verify(queue).schedule2(eq(task2), anyInt(), eq(Collections.emptyList()));
-        verifyNoMoreInteractions(queue);
-    }
+  @Test
+  void taskCompleted_should_resubmit_task_for_all_executors() {
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verify(queue).schedule2(eq(task1), anyInt(), eq(Collections.emptyList()));
+    verify(queue).schedule2(eq(task2), anyInt(), eq(Collections.emptyList()));
+    verifyNoMoreInteractions(queue);
+  }
 
-    @Test
-    void taskCompleted_should_abort_executors_during_resubmit() {
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-                .afterDisconnect(computer, taskListener);
-        verify(executor1).interrupt(Result.ABORTED, new EC2ExecutorInterruptionCause("i-12"));
-        verify(executor2).interrupt(Result.ABORTED, new EC2ExecutorInterruptionCause("i-12"));
-    }
+  @Test
+  void taskCompleted_should_abort_executors_during_resubmit() {
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verify(executor1).interrupt(Result.ABORTED, new EC2ExecutorInterruptionCause("i-12"));
+    verify(executor2).interrupt(Result.ABORTED, new EC2ExecutorInterruptionCause("i-12"));
+  }
 
-    @Test
-    void taskCompleted_should_resubmit_task_with_actions() {
-        when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
-        //when(executable1.getActions()).thenReturn(Arrays.asList(action1));
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-                .afterDisconnect(computer, taskListener);
-        verify(queue).schedule2(eq(task1), anyInt(), eq(/* Arrays.asList(action1) */ Collections.emptyList()));
-        verifyNoMoreInteractions(queue);
-    }
+  @Test
+  void taskCompleted_should_resubmit_task_with_actions() {
+    when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
+    // when(executable1.getActions()).thenReturn(Arrays.asList(action1));
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verify(queue).schedule2(eq(task1), anyInt(), eq(/* Arrays.asList(action1) */ Collections.emptyList()));
+    verifyNoMoreInteractions(queue);
+  }
 
-    @Test
-    void taskCompleted_should_resubmit_task_with_failed_build_actions() {
-        when(subTask1.getOwnerTask()).thenReturn(workflowJob);
-        when(workflowJob.getLastFailedBuild()).thenReturn(workflowRun);
-        when(workflowRun.getActions(any())).thenReturn((Collections.singletonList(action1)));
-        when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
-        new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
-            .afterDisconnect(computer, taskListener);
-        verify(queue).schedule2(eq(workflowJob), anyInt(), eq(Arrays.asList(action1)));
-        verify(workflowRun, times(1)).getActions(any());
-    }
+  @Test
+  void taskCompleted_should_resubmit_task_with_build_actions() {
+    when(subTask1.getOwnerTask()).thenReturn(workflowJob);
+    when(workflowJob.getLastBuild()).thenReturn(workflowRun);
+    when(workflowRun.getActions(any())).thenReturn((Collections.singletonList(action1)));
+    when(computer.getExecutors()).thenReturn(Arrays.asList(executor1));
+    new EC2FleetAutoResubmitComputerLauncher(baseComputerLauncher)
+        .afterDisconnect(computer, taskListener);
+    verify(queue).schedule2(eq(workflowJob), anyInt(), eq(Arrays.asList(action1)));
+    verify(workflowRun, times(1)).getActions(any());
+  }
 
 }
